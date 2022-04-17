b49db85b7be17a333d9bbfd07e3cfd2c67aacecb
arashtajdar <arashtajdar@gmail.com>
2022-04-17 12:54:06 +0200

    Add form to PostComponent

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,12 +1,24 @@
 <template>
 <div>
   <h1>Post is here</h1>
+  <form @submit="PostData" method="post">
+      <input type="text" name="name"> <br><br>
+      <input type="text" name="author"> <br><br>
+      <button type="submit">Create book</button>
+  </form>
 </div>
 </template>
 
 <script>
 export default {
-name: "PostComponent"
+name: "PostComponent",
+  data(){},
+  methods:{
+      PostData(){
+
+    }
+
+  }
 }
 </script>
 
